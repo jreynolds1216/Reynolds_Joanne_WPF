170b1d1a8cf0ccd43ffa6cfaa7c2f99e69d1b327
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 14:42:11 -0500

    Added calculations for how many slices of pizza each person can have at a party.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -8,3 +8,17 @@ var ageInDogYears = ageInHumanYears * 7;
 
 //Print the output age.
 console.log("Sparky is" + " " + ageInHumanYears + " " + "human years old which is" + " " + ageInDogYears + " " + "in dog years.")
+
+
+
+//Calculate the number of slices of pizza each person can have at the party.
+
+var slicesPerPizza = 8;
+var numberOfPeople = 4;
+var numberOfPizzas = 1;
+
+//Multiply the number of pizzas by the number of slices then divide by the number of people at the party.
+var numberOfSlicesPerPerson = numberOfPizzas * slicesPerPizza / numberOfPeople;
+
+//Print the output.
+console.log("Each person ate" + " " + numberOfSlicesPerPerson + " " + "slices of pizza at the party.")
