5810911e75293910065d3b3725addfa02349fb11
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 14:19:38 -0500

    Added calculation for dog's age from human years to dog years.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,1 +1,10 @@
 //Joanne Reynolds August 15th, 2013 Expressions Worksheet
+
+//Calculate Sparky's age in dog years
+
+var ageInHumanYears = 5;
+//Multiply age in humans years times 7.
+var ageInDogYears = ageInHumanYears * 7;
+
+//Print the output age.
+console.log("Sparky is" + " " + ageInHumanYears + " " + "human years old which is" + " " + ageInDogYears + " " + "in dog years.")
