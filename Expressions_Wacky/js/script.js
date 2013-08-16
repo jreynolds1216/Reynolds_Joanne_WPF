533aa6f617fb789d5f4a0111d0859e792e21eea1
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 21:54:50 -0500

    Tested the calculations with subtraction and it works. Still can't get addition to work for some reason.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,2 +1,13 @@
 //Joanne Reynolds August 16th, 2013 Expressions_Wacky
 
+//Calculate the average time your dog spent outside for three days.
+
+var one = prompt("We are calculating the average time your dog spent outside in 3 days. \nPlease enter the number of minutes for Day 1:");
+
+var two = prompt("Please enter the number of minutes for Day 2:");
+
+var three = prompt("Please enter the number of minutes for Day 3:");
+
+var totalMinutes = one-two-three;
+
+console.log(totalMinutes);
