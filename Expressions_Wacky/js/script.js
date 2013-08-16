de56802805cfd8efa44d6a71198d6222999d5ee4
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 22:08:25 -0500

    Went through and finished writing the calculations and adding comments. Changed back to addition even though it still didn't work. I tried everything I could think of. Need to come back to this and fix it.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,12 +2,18 @@
 
 //Calculate the average time your dog spent outside for three days.
 
+//Created prompts for the user to enter how long the dog was outside for three different days.
 var one = prompt("We are calculating the average time your dog spent outside in 3 days. \nPlease enter the number of minutes for Day 1:");
 
 var two = prompt("Please enter the number of minutes for Day 2:");
 
 var three = prompt("Please enter the number of minutes for Day 3:");
 
-var totalMinutes = one-two-three;
+//Added all three days together first then divided by 3 to get the average.
+var totalMinutes = (one+two+three)/3;
 
-console.log(totalMinutes);
+//Created an alert for the user to see the average number of minutes.
+alert("Your dog spent an average of" + " " + totalMinutes + " " + "minutes outside in three days.")
+
+//Created a message with answer for the console.
+console.log("Your dog spent an average of" + " " + totalMinutes + " " + "minutes outside in three days.");
