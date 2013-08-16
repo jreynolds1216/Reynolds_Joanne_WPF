492f60232d29e71c435b5c2d7417f70f90050487
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 19:33:46 -0500

    Defined what we are calculating and added prompt to enter the number of technicians available.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,6 @@
 //Joanne Reynolds August 16th, 2013 Expressions_Industry
+
+
+//Calculate the number of service jobs that need to be done per hour by the service technicians at our dealership.
+
+var numberOfTechs = prompt("We are calculating the number of jobs the technicians need to do per hour to get all jobs done in the alloted number of hours. Please enter the number of technicians working:");
