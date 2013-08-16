8bb947186a3d7615db976de6f0043d93aa04db9e
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 20:14:31 -0500

    Made the first prompt telling the user what we are calculating and to enter the number of licks for the first tootsie pop.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,5 @@
 //Joanne Reynolds August 16th, 2013 Expressions_Wacky
 
 //Calculate the average number of licks it takes to get to the center of a tootsie pop.
+
+var numberOfLicks1 = prompt("We are calculating the average number of licks it takes to get to the center of a tootsie pop. \nPlease enter the number of licks for the first tootsie pop:");
