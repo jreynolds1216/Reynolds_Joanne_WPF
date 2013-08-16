40229232459572872e7b2a9c5886b312487ddeb2
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 21:24:55 -0500

    Deleted the problem I had. For some reason I couldn't get the addition to work at all. I tried everything I could. Going to try something else to see if I can get it.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,12 +1,2 @@
 //Joanne Reynolds August 16th, 2013 Expressions_Wacky
 
-//Calculate the average number of licks it takes to get to the center of a tootsie pop.
-
-//I added four prompts for the user to input the number of licks for four different tootsie pops.
-var numberOfLicks1 = prompt("We are calculating the average number of licks it takes to get to the center of a tootsie pop. \nPlease enter the number of licks for the first tootsie pop:");
-
-var numberOfLicks2 = prompt("Please enter the number of licks for the second tootsie pop:");
-
-var numberOfLicks3 = prompt("Please enter the number of licks for the third tootsie pop:");
-
-var numberOfLicks4 = prompt ("Please enter the number of licks for the fourth tootsie pop:");
