bdd1b7cbb174901c5f95c2324054e901ea2c471d
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 19:39:00 -0500

    Added comments to my first two prompts.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,6 +3,8 @@
 
 //Calculate the number of service jobs that need to be done per hour by the service technicians at our dealership.
 
+//First we need to know how many techs are working so I added a prompt for that.
 var numberOfTechs = prompt("We are calculating the number of jobs the technicians need to do per hour to get all jobs done in the allotted number of hours. Please enter the number of technicians working:");
 
+//I added a prompt to enter how many jobs we need to get done.
 var numberOfJobs = prompt("Please enter the number of jobs that need to be done:");
