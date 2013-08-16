ae39c191fb608fae9c6efabaa18eacdb465db666
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 20:46:53 -0500

    Added a prompt asking the user to input the number of cans each cat eats per day.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,5 @@
 //Calculate how many cans of cat food you will need to feed your cats for a certain number of days.
 
 var cats = prompt("We are calculating the number of cans of cat food needed to feed your cats for a certain number of days. \nPlease enter the number of cats you are feeding:");
+
+var cansOfFood = prompt("Please enter the number of cans each cat eats per day:");
