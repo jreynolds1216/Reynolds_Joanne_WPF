8d1b7fd12683268ba5e7fd0db3ef75a14aeec9b1
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-16 09:26:23 -0500

    Added *1 to all three of my prompts to change the strings to integers. Tested the calculations and it works.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,11 +3,11 @@
 //Calculate the average time your dog spent outside for three days.
 
 //Created prompts for the user to enter how long the dog was outside for three different days.
-var one = prompt("We are calculating the average time your dog spent outside in 3 days. \nPlease enter the number of minutes for Day 1:");
+var one = prompt("We are calculating the average time your dog spent outside in 3 days. \nPlease enter the number of minutes for Day 1:")*1;
 
-var two = prompt("Please enter the number of minutes for Day 2:");
+var two = prompt("Please enter the number of minutes for Day 2:")*1;
 
-var three = prompt("Please enter the number of minutes for Day 3:");
+var three = prompt("Please enter the number of minutes for Day 3:")*1;
 
 //Added all three days together first then divided by 3 to get the average.
 var totalMinutes = (one+two+three)/3;
