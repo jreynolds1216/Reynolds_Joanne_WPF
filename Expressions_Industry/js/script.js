a1ffac7298f832d5ce87e1420ef2f70cb31d73cc
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-15 19:42:01 -0500

    I added a comment to my third prompt.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,4 +9,5 @@ var numberOfTechs = prompt("We are calculating the number of jobs the technician
 //I added a prompt to enter how many jobs we need to get done.
 var numberOfJobs = prompt("Please enter the number of jobs that need to be done:");
 
+//I added a prompt to enter how many hours the techs have to get all of the jobs done.
 var numberOfHours = prompt("Please enter the number of hours the technicians have to get the jobs done:");
