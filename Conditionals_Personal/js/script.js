181a2e9bf44d78a1b4f65768c289125c4cdfd193
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-22 15:21:58 -0500

    Added a conditional for if the grass is more than 5 inches and it isn't raining the user will get a message saying they need to cut the grass.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,3 +7,8 @@ var grass = prompt("We are going to determine if we need to cut the grass today 
 
 //Added a prompt asking the user to enter whether or not it is raining.
 var rain = prompt("Is it raining today? Enter yes or no.");
+
+//Added a conditional for if the grass is greater than or equal to 5 inches and it isn't raining. The user will get a message saying they need to cut the grass.
+if (grass >= 5 && rain === "no"){
+    console.log("You need to cut the grass today! Your yard looks like a jungle.");
+}
