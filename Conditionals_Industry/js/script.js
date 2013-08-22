66256e9c90c4eea72160c7be041133706eb1b85e
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-22 14:38:35 -0500

    Added a comment about what we are going to be calculating and then added the first prompt for the user to enter their vehicle weight in pounds.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,7 @@
 //Joanne Reynolds August 22, 2013 Conditionals Industry
+
+//We are going to determine whether or not a vehicle can go on the lift in the service department. We will determine this by using the weight and length(in feet) of the vehicle.
+
+//Create a prompt for the user to enter how heavy the car is(in pounds).
+
+var weight = prompt("We are going to determine if your vehicle can go on the lift in our service deparment. Enter your vehicle's weight in pounds:");
