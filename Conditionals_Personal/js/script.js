447d3321c69e4989f649511da222a112e52eeed2
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-22 15:18:23 -0500

    Added a comment and a prompt. Prompt explains to the user what we will be calculating and asks them to input how tall their grass is in inches.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,6 @@
 //Joanne Reynolds August 22, 2013 Conditionals Personal
 
 //We will be trying to determine if we need to cut our grass today. If it is more than 5 inches and not raining we will cut it. If it is raining or less than 5 inches we won't cut it.
+
+//Added a prompt explaining what we are calculating to the user and asking them how tall their grass is in inches.
+var grass = prompt("We are going to determine if we need to cut the grass today or not. Enter the height of the grass in inches:");
