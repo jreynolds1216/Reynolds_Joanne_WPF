d2e38fc29867dd00291dc97bbc21476986e209cf
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-22 14:31:12 -0500

    Added my name, date, and assignment name at the top.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,7 +1,1 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jorey1687
- * Date: 8/22/13
- * Time: 2:26 PM
- * To change this template use File | Settings | File Templates.
- */
+//Joanne Reynolds August 22, 2013 Conditionals Industry
