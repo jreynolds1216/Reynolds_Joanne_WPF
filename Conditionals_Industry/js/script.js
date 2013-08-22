8d01e3983de8774c07f94904d79905727d384dc1
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-22 14:50:25 -0500

    Added a comment explaining the ternary and then added the ternary. Tested all the different options to make sure the console produced the correct message.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -13,5 +13,7 @@ var length = prompt("Please enter your vehicle's length in feet:");
 //Added a string for your vehicle so that I can make the conditional into a ternary.
 var vehicle;
 
-
+//Added a ternary in which if the customer's vehicle weights less than 4000 pounds and the length is less than 20 feet they get a message saying their car can go on the lift. If one or both of the pieces are false then they get a message saying they must service their vehicle elsewhere.
+vehicle = (weight < 4000 && length < 20) ? "Your car can go on the lift." : "Sorry, you must service your vehicle elsewhere.";
+console.log(vehicle);
 
