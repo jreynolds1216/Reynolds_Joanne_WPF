0b5a443a40697eddc9a41dc1b186e7c44c987716
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-21 23:49:45 -0500

    Initial set up of conditionals worksheet.

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,6 @@
+//Joanne Reynolds August 21, 2013 Conditionals Worksheet
+
+//Problem #1: Stuff Your Face
+
+//Competitor must weight 250 pounds or more to compete in heavyweight. If yes, console says "The competitor qualifies for the heavyweight division." If no, console says "The competitor needs to gain some weight!"
+
