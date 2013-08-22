c4609bcd9eba49a10850abe771e82c2e32da533a
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-22 14:47:35 -0500

    Added var vehicle so that I can use it in the ternary.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,8 +4,14 @@
 
 //Create a prompt for the user to enter how heavy the car is(in pounds).
 
-var weight = prompt("We are going to determine if your vehicle can go on the lift in our service deparment. Enter your vehicle's weight in pounds:");
+var weight = prompt("We are going to determine if your vehicle can go on the lift in our service department. Enter your vehicle's weight in pounds:");
 
 //Create a prompt for the user to enter how long the car is(in feet).
 
 var length = prompt("Please enter your vehicle's length in feet:");
+
+//Added a string for your vehicle so that I can make the conditional into a ternary.
+var vehicle;
+
+
+
