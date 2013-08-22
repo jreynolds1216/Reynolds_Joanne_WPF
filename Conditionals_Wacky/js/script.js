568b383b2ee164ff39df2453333d5f5853a1c54f
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-22 16:07:31 -0500

    Added my name, date, and assignment name to js file.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,7 +1,3 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jorey1687
- * Date: 8/22/13
- * Time: 4:03 PM
- * To change this template use File | Settings | File Templates.
- */
+//Joanne Reynolds August 22, 2013 Conditionals Wacky
+
+
