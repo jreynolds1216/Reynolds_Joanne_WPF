9fb75bad538ad6b80427edf450d922c741a83803
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-22 15:16:46 -0500

    Added a comment explaining what we will be calculating.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,2 +1,3 @@
 //Joanne Reynolds August 22, 2013 Conditionals Personal
 
+//We will be trying to determine if we need to cut our grass today. If it is more than 5 inches and not raining we will cut it. If it is raining or less than 5 inches we won't cut it.
