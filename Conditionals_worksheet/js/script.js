c5665094cc272522cb751b09ae73240ddbd0fdab
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-21 23:56:21 -0500

    Created var and set to 250 initially and made statement that if weight was greater than or equal to in console it would say competitor qualifies. I tested it out and it worked. I then changed weight to 220 and added else competitor needs to gain weight and tested to make sure it also worked.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,3 +4,10 @@
 
 //Competitor must weight 250 pounds or more to compete in heavyweight. If yes, console says "The competitor qualifies for the heavyweight division." If no, console says "The competitor needs to gain some weight!"
 
+var weight = 220;
+
+if (weight >= 250) {
+    console.log("The competitor qualifies for the heavyweight division.");
+}else{
+    console.log("The competitor needs to gain some weight!");
+}
