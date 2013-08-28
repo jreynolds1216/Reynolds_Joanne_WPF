1f6983fc6a2bf2c248e507b9d15c1c57fadc48e8
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-27 21:20:56 -0500

    Added function to calculate the circumference of a circle. Tested a few different times to make sure answer is correct in console log.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,1 +1,15 @@
 //Joanne Reynolds August 27, 2013 Functions worksheet
+
+
+//Function to calculate the circumference of a circle.
+
+var total = totalCirc (12);
+function totalCirc(radius){
+    var circ = (radius * 2) * 3.14;
+    return circ;
+}
+
+console.log(total);
+
+
+
