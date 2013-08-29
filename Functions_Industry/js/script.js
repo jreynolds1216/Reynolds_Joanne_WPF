c008e8eaa14976d23f329ee9f84c0682ab36be2d
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 19:44:39 -0500

    Went back and commented each step that I did.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,8 +2,10 @@
 
 //We are going to be creating a function in which we determine if we can service your car based on what type of car you have and what year it is.
 
+//Created variable service for my function. I tried to just call on function service department but couldn't get it to work? This did though so I stuck with it.
 var service = serviceDept();
 
+//Created function with two parameters(typeOfCar and year). I made prompts for user to enter both pieces of information then created a ternary that figured out whether or not we could service the car.
 function serviceDept(typeOfCar, year){
     var typeOfCar = prompt("We are going to find out if we can service your car. First, please enter if your car is foreign or american:");
     var year = prompt("Please enter what year your car is:");
@@ -12,5 +14,7 @@ function serviceDept(typeOfCar, year){
     return car;
 
 }
+//Created message with the answer for the console.
 console.log(service);
+//Also created alert so user can see answer right away instead of looking at the console.
 alert(service);
