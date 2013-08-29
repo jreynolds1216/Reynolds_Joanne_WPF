1eeefae18b9ad5c6b8310828e5241ce83218ee2e
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 21:29:47 -0500

    Went back and commented all of my steps for my problem.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,7 +1,9 @@
 //Joanne Reynolds August 28, 2013 Functions Wacky
 
 
+//We are going to create a function to determine whether or not you can drink your milk based on the date and the freshness.
 
+//Created a function named milk with two parameters(milkDate and milkOpened). Prompted the user to answer two questions to determine whether or not the milk was drinkable. Created and else if statement that gives different alerts and console messages depending on what the user answered in the prompts.
 function milk(milkDate, milkOpened){
     var milkDate = prompt("We are going to determine whether or not you can drink your milk. Has the date on the milk passed? Please enter yes or no:");
     var milkOpened = prompt("Was the milk opened more than 7 days ago? Please enter yes or no:");
@@ -18,4 +20,5 @@ function milk(milkDate, milkOpened){
     }
 }
 
+//Invoked my function so that it runs through the prompts and gives the user the appropriate alerts and message in the console.
 milk();
