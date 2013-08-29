26eb8a531e745792ffcc10dbafc61485f2162088
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 19:15:28 -0500

    Added my name, date, and assignment name.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,7 +1,3 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jorey1687
- * Date: 8/28/13
- * Time: 6:58 PM
- * To change this template use File | Settings | File Templates.
- */
+//Joanne Reynolds August 28, 2013 Functions Industry
+
+
