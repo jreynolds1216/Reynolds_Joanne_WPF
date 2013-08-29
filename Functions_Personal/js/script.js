a193809d58bc45cdf7ce152b8a90116bdef1cd86
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 20:31:33 -0500

    Created an anonymous function to calculate the number of sandwiches needed total per day to feed everyone on the trip. Created three variables with prompts for the user to enter the information(parameters).

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,3 +1,13 @@
 //Joanne Reynolds August 28, 2013 Functions Personal
 
-//We are going to calculate how many sandwiches we need for each person on a road trip depending on the # of sandwiches each person will eat per day and how many days we will be on the trip.
+//We are going to calculate how many sandwiches we need for each day on a road trip depending on the # of sandwiches each person will eat per day and how many days we will be on the trip.
+
+var trip = function(people, perDay, days){
+    var people = prompt("We are going to be calculating how many sandwiches total we need per day on our road trip. Please enter how many people will be going on the trip:");
+    var perDay = prompt("How many sandwiches will each person eat per day?");
+    var days = prompt("How many days will the road trip last?");
+    var sandwiches = people * perDay / days;
+    return sandwiches;
+}
+
+
