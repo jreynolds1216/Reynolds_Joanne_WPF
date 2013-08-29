9b5df53c7008ce8b42c79955e29328fc8f7ae0e2
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 21:18:36 -0500

    Created the function with two parameters in which the user tell us whether the milk is expired or was open more than 7 days ago. If so, they can't drink it and if both of those are false then they can. Printed message to console. I get correct answer but underneath it says my console line in my js file is undefined. Can't figure out what to change to fix it. Will keep trying!!

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,1 +1,19 @@
 //Joanne Reynolds August 28, 2013 Functions Wacky
+
+
+
+function milk(milkDate, milkOpened){
+    var milkDate = prompt("We are going to determine whether or not you can drink your milk. Has the date on the milk passed? Please enter yes or no:");
+    var milkOpened = prompt("Was the milk opened more than 7 days ago? Please enter yes or no:");
+
+    if(milkDate === "yes"){
+        console.log("Ew, throw the milk away. You cannot drink it.");
+    }else if(milkOpened === "yes"){
+        console.log("This milk probably isn't fresh. I would advise you not to drink it.");
+    }else{
+        console.log("The milk is fresh and the date is current. Pour yourself a giant glass!");
+    }
+}
+
+var drinkingMilk = milk();
+console.log(drinkingMilk);
