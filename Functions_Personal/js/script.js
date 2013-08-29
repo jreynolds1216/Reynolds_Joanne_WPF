ce58208248fabed68986f1af565e429cdd6694ea
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 20:20:29 -0500

    Commented what we will be calculating in the problem.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,2 +1,3 @@
 //Joanne Reynolds August 28, 2013 Functions Personal
 
+//We are going to calculate how many sandwiches we need for each person on a road trip depending on the # of sandwiches each person will eat per day and how many days we will be on the trip.
