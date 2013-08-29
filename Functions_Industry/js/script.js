e75f7c878134abc79aaf5e26af01651955e90991
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 19:34:49 -0500

    Amended my ternary to include message that car may be too old and that is why we won't service it. Also added service as a variable for my function. Created message for console that calls on the variable with my function. Tested the function several times and it worked correctly.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,10 +2,14 @@
 
 //We are going to be creating a function in which we determine if we can service your car based on what type of car you have and what year it is.
 
+var service = serviceDept();
+
 function serviceDept(typeOfCar, year){
     var typeOfCar = prompt("We are going to find out if we can service your car. First, please enter if your car is foreign or american:");
     var year = prompt("Please enter what year your car is:");
     var car;
-    car = (typeOfCar == "american" && year >= 1990) ? "We would be happy to service your car.":"Sorry, we do not service foreign cars.";
+    car = (typeOfCar == "american" && year >= 1990) ? "We would be happy to service your car.":"Sorry, we do not service foreign cars and/or your car is too old.";
+    return car;
 
 }
+console.log(service);
