b99068225b198dbdc9be73c0c7f1375539fcc3a1
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 20:12:57 -0500

    Added my name, date, and assignment name on file.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,7 +1,2 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jorey1687
- * Date: 8/28/13
- * Time: 7:47 PM
- * To change this template use File | Settings | File Templates.
- */
+//Joanne Reynolds August 28, 2013 Functions Personal
+
