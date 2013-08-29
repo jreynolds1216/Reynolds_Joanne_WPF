430a7bb3fa32d9c83502cf615fc959f7e2292d77
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 19:17:56 -0500

    Explained what our function will be, named my function and the parameters.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,7 @@
 //Joanne Reynolds August 28, 2013 Functions Industry
 
+//We are going to be creating a function in which we determine if we can service your car based on what type of car you have and what year it is.
 
+function serviceDept(typeOfCar, year){
+
+}
