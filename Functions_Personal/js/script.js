9cbc7f5b3e28cdb03a771fe86a17ff661a4b6285
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 20:35:55 -0500

    Commented my function and explained all the pieces of it. Created a variable for my function. I tried to just call on my function name but once again I couldn't get it to work.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,6 +2,7 @@
 
 //We are going to calculate how many sandwiches we need for each day on a road trip depending on the # of sandwiches each person will eat per day and how many days we will be on the trip.
 
+//Created a function with three parameters. Prompted the user to enter the # of people on the trip, the # of sandwiches they will eat per day, and the # of days we will be on the trip. Multiplied the # of people by the amount they will each per day and then divided that # by the # of days the trip will last. Returned the answer in the variables sandwiches.
 var trip = function(people, perDay, days){
     var people = prompt("We are going to be calculating how many sandwiches total we need per day on our road trip. Please enter how many people will be going on the trip:");
     var perDay = prompt("How many sandwiches will each person eat per day?");
@@ -10,4 +11,5 @@ var trip = function(people, perDay, days){
     return sandwiches;
 }
 
-
+//Created a variable for my function. Same thing as last problem. I could not get it to work without doing this?
+var vacationTrip = trip();
