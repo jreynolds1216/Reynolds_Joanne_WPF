a8f68a0c494c0bd6f4bd83471914fd5288b83ddf
Joanne Reynolds <jreynolds1216@fullsail.edu>
2013-08-28 19:19:25 -0500

    Declared my first variable (typeOfCar).

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,5 +3,6 @@
 //We are going to be creating a function in which we determine if we can service your car based on what type of car you have and what year it is.
 
 function serviceDept(typeOfCar, year){
+    var typeOfCar = prompt("We are going to find out if we can service your car. First, please enter if your car is foreign or american");
 
 }
